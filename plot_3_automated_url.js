0e16503901a63d8a196dd327ce17d1c635c85c2c
Jonathan-Edwall <edwalljonathan@gmail.com>
2023-11-15 17:02:20 +0100

    Update plot_3_automated_url.js

--- plot_3_automated_url.js
+++ plot_3_automated_url.js
@@ -1,34 +1,40 @@
-// plot_3_automated_url.js
-
 var plot_spec = {
     "title": "Plot 3",
     "subtitle": "Example",
+    //"static": false,
+    //"layout": { type: 'linear', },
     "xDomain": { "interval": [0, 200000] },
     "alignment": "overlay",
     "width": 1000,
     "height": 200,
     "assembly": "unknown",
-    "views": [
+    "tracks": [
         {
-            "tracks": [
-                {
-                    "data": {
-                        "url": "",
-                        "type": "csv",
-                        "column": "START",
-                        "value": "all_PI",
-                        "sampleLength": "100000",
-                    },
-                    "mark": "line",
-                    "x": { "field": "START", "type": "genomic", "axis": "bottom" },
-                    "y": { "field": "all_PI", "type": "quantitative", "axis": "left", "domain": [0, 1] },
-                    "color": { "value": "red" },
-                    "opacity": { "value": 0.5 },
-                    "size": { "value": 2 },
-                },
-            ],
-        },
-    ],
-};
+            // "data": {
+            //     "url": "C:\Users\jonat\GitHub\Visualizing-adaptive-variation\data\ALL.at_vs_me.consolidated_windows.1000bp.GT.seq_s_18363.csv",
+            //     "type": "csv",
+            //     "column": "START",
+            //     "value": "all_PI",
+            //     "sampleLength": "100000",
+            // },
 
+            "data": {
+                "url": "",
+                "type": "csv",
+                "column": "START",
+                "value": "all_PI",
+                "sampleLength": "100000",
+            },
+
+            "mark": "line",
+            "x": { "field": "START", "type": "genomic", "axis": "bottom" },
+            "y": { "field": "all_PI", "type": "quantitative", "axis": "left", "domain": [0, 1] },
+            "color": { "value": "red" },
+            "opacity": { "value": 0.5 },
+            "size": { "value": 2 },
+
+        },
+    ]
+};
+                    
 export { plot_spec };
