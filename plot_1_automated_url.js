bffbe60ea2c6757482cf5e172d414c941a08b46a
Jonathan-Edwall <92929542+Jonathan-Edwall@users.noreply.github.com>
2023-11-16 00:17:31 +0100

    added html script that plots the file the user picks (Gosling_1_plot_automated_fileselect.html)
    Gosling_1_plot_automated_fileselect.html lets the user pick a file it wants to see and gets 1 corresponding plot from it.
    
    It currently displays FST and should work for any snps-file in data/gene_regions, that has been converted from .tsv to .csv

---
+++ plot_1_automated_url.js
@@ -0,0 +1,33 @@
+var plot_spec = {
+    "title": "Plot 3",
+    "subtitle": "Example",
+    //"static": false,
+    //"layout": { type: 'linear', },
+    "xDomain": { "interval": [0, 200000] },
+    "alignment": "overlay",
+    "width": 1000,
+    "height": 200,
+    "assembly": "unknown",
+    "tracks": [
+        {
+            "data": {
+                "url": "",
+                "type": "csv",
+                "column": "POS",
+                "value": "FST",
+                "binSize": 10,
+                "sampleLength": "100000",
+            },
+            "mark": "point",
+            "x": { "field": "POS", "type": "genomic", "axis": "bottom" },
+            "y": { "field": "FST", "type": "quantitative", "axis": "none", "domain": [0, 1] },
+            "color": { "field": "TYPE", "type": "nominal", "legend": true },
+            "opacity": { "value": 0.8 },
+            "size": { "value": 4 },
+  
+
+        },
+    ]
+};
+                    
+export { plot_spec };
