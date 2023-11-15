9938bcd87bf79ff405cc8a219e1d52e9209f17ab
Jonathan-Edwall <92929542+Jonathan-Edwall@users.noreply.github.com>
2023-11-15 12:21:53 +0100

    By starting the VS code plugin "Live Server" and then stepping into the directory of the data through the main directory of the server (on the browser), the data can be used as an URL, which is necessairy for gosling.js!

---
+++ plot_3.js
@@ -0,0 +1,40 @@
+var plot_spec = {
+    "title": "Plot 3",
+    "subtitle": "Example",
+    //"static": false,
+    //"layout": { type: 'linear', },
+    "xDomain": { "interval": [0, 200000] },
+    "alignment": "overlay",
+    "width": 1000,
+    "height": 200,
+    "assembly": "unknown",
+    "tracks": [
+        {
+            // "data": {
+            //     "url": "C:\Users\jonat\GitHub\Visualizing-adaptive-variation\data\ALL.at_vs_me.consolidated_windows.1000bp.GT.seq_s_18363.csv",
+            //     "type": "csv",
+            //     "column": "START",
+            //     "value": "all_PI",
+            //     "sampleLength": "100000",
+            // },
+
+            "data": {
+                "url": "http://127.0.0.1:5500/data/ALL.at_vs_me.consolidated_windows.1000bp.GT.seq_s_18363.csv",
+                "type": "csv",
+                "column": "START",
+                "value": "all_PI",
+                "sampleLength": "100000",
+            },
+
+            "mark": "line",
+            "x": { "field": "START", "type": "genomic", "axis": "bottom" },
+            "y": { "field": "all_PI", "type": "quantitative", "axis": "left", "domain": [0, 1] },
+            "color": { "value": "red" },
+            "opacity": { "value": 0.5 },
+            "size": { "value": 2 },
+
+        },
+    ]
+};
+                    
+export { plot_spec };
