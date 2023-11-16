d2bb796bd64db4b0de8ac3d4af4bf0b852aee8a4
matildasofia <matilda.styfberg@gmail.com>
2023-11-16 09:59:48 +0100

    Some files for trying the real data

--- automated_url.js
+++ automated_url.js
@@ -1,4 +1,6 @@
-var spec = {
+// plot_3_automated_url.js
+
+var plot_spec = {
     "title": "Mapping depth",
     "subtitle": "A first plot by Matilda",
     "xDomain": {"interval": [0, 270000] },
@@ -9,12 +11,12 @@ var spec = {
         "width": 800,
         "height": 180,
         "data": {
-          "url": "https://export.uppmax.uu.se/uppstore2018098/genomelens/data/test/at_vs_me.csv.gz.genic.seq_s_18363.csv",
+          //"url": "https://export.uppmax.uu.se/uppstore2018098/genomelens/data/test/at_vs_me.csv.gz.genic.seq_s_18363.csv",
           //"url": "./gosling/gene_regions.data_mappings/seq_s_18363.COM_SPAL_1_010415_XLOC_038300.mappings.windows_100.at.tsv",
           //"url": "http://localhost:8888/gosling/gene_regions.data_mappings/seq_c_23164.REF_STRG_1_64511_XLOC_008442.mappings.windows_100.at.tsv",
           //"url": "http://127.0.0.1:5500/gosling/gene_regions.data_mappings/seq_c_23164.REF_STRG_1_64511_XLOC_008442.mappings.windows_100.at.tsv",
           //"url": "./Users/matildastyfberg/Desktop/UppsalaUniversitet/Applied_bioinformatics/Repository/Visualizing-adaptive-variation/gosling/gene_regions/seq_c_23164.REF_STRG_1_64511_XLOC_008442.snps.tsv",
-          //"url": "",
+          "url": "",
           "type": "csv",
           "column": "POS",
           "value": "FST",
@@ -27,6 +29,6 @@ var spec = {
       }
     ],
     "responsiveSize": false
-  } 
+};
 
-export { spec }; 
+export { plot_spec };
