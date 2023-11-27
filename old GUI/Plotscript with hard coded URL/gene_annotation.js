d700620879c80a89230489f1210a778c98c1b8ca
matildasofia <matilda.styfberg@gmail.com>
2023-11-27 11:30:54 +0100

    Try of gene annotation plot, not finished

---
+++ old GUI/Plotscript with hard coded URL/gene_annotation.js
@@ -0,0 +1,66 @@
+const size_val = 1.5;
+const opacity_AT = 1;
+const opacity_ME = 0.4;
+const color_td_AT = "red"
+const color_td_ME = "red"
+
+
+var plot_spec = {
+    "static": false,
+    //"layout": { "type": "linear" },
+    "xDomain": { "interval": [0, 250000] },
+    "alignment": "overlay",
+    "width": 1100,
+    "height": 300,
+    "assembly": "unknown",
+
+    // Specifying Light gray color as background color
+    "style": {"background":"#D3D3D3", "backgroundOpacity":0.1},
+
+    "tracks": [
+        {
+        // ¤¤¤¤¤¤¤¤¤ Atlantic  ¤¤¤¤¤¤¤¤¤
+            "data": {
+                "url": ".../data/genes/seq_c_23164.REF_STRG_1_64511_XLOC_008442.gff",
+                // seq_c_23164.REF_STRG_1_64511_XLOC_008442
+                "type": "csv",
+                "separator": "\t",
+                "column": "POS",
+                "value": "at_TD",
+                "sampleLength": "100000", 
+            },
+            "mark": "line",
+            "x": { "field": "POS", "type": "genomic", "axis": "bottom", "linkingId": "link-1" },
+            "y": { "field": "at_TD", "type": "quantitative", "axis": "left", "domain": [-2.5, 2.5] }, 
+            "color": { "value": color_td_AT }, // Color for td
+            "opacity": { "value": opacity_AT}, // Opacity for Atlantic
+            "size": { "value": size_val }, //the width of the line
+            "tooltip": [
+        {"field": "at_TD", "type": "quantitative", "format":"0.2f","alt":"Atlantic - Tajima's D (TD):"}
+                        ],
+        },
+        // ¤¤¤¤¤¤¤¤¤ Mediterranean  ¤¤¤¤¤¤¤¤¤
+        {
+            "data": {
+                "url": "",
+                "type": "csv",
+                "separator": "\t",
+                "column": "POS",
+                "value": "me_TD",
+                "sampleLength": "100000", 
+            },
+            "mark": "line",
+            "x": { "field": "POS", "type": "genomic", "axis": "none", "linkingId": "link-1" },
+            "y": { "field": "me_TD", "type": "quantitative", "axis": "left", "domain": [-2.5, 2.5] }, 
+            "color": { "value": color_td_ME }, // Color for td
+            "opacity": { "value": opacity_ME}, // Opacity for Mediterranean
+            "size": { "value": size_val }, //the width of the line
+            "tooltip": [
+        {"field": "me_TD", "type": "quantitative", "format":"0.2f","alt":"Mediterranean - Tajima's D (TD):"}
+                    ],
+        
+        }
+
+    ]
+};                   
+export { plot_spec };
