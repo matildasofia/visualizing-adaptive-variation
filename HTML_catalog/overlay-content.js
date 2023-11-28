bf0686185813d20bef1a001c30717b2b6466649d
Moa Qvarnlöf <moa@qvarnlof.se>
2023-11-28 18:17:31 +0100

    html catalog

---
+++ HTML_catalog/overlay-content.js
@@ -0,0 +1,27 @@
+export async function addOverlay(container) {
+    container.innerHTML = `
+    <div class="overlay" style="margin-top: 20em;">
+            <p><b>Atlantic</b><br>
+                Darker colors:
+                <div class="circles">
+                    <div class="circle" style="background-color: rgba(255, 0, 0, 1);"></div>
+                    <div class="circle" style="background-color: blue;"></div>
+                    <div class="circle" style="background-color: darkorange;"></div>
+                    <div class="circle" style="background-color: purple;"></div>
+                    <div class="circle" style="background-color: green;"></div>
+                </div>
+            </p>
+            <p>
+                <b>Mediterranian</b><br>
+                    Lighter colors:
+                    <div class="circles">
+                        <div class="circle" style="background-color: rgba(255, 0, 0, 0.4);"></div>
+                        <div class="circle" style="background-color: lightblue;"></div>
+                        <div class="circle" style="background-color: gold"></div>
+                        <div class="circle" style="background-color: violet;"></div>
+                        <div class="circle" style="background-color: lightgreen;"></div>
+                    </div>
+            </p>
+        </div>`
+
+}
